8222b23ee08ff2fa9ba0eb47f2b5772f42091df8
unknown <abhisheksrivastava9719@gmail.com>
2022-07-17 12:11:17 +0530

    [Abhishek] Added employee Wage Problem using OOPs Concepts

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
 ﻿using EmployeeWage;
 Console.WriteLine("Welcome to Employee Wage Computation program");
-Console.WriteLine("Enter 1 - Part time & 2- for full time employee");    //either part time or full time employee
-int choice = Convert.ToInt16(Console.ReadLine());
-if (choice < 1 || choice > 2)
-{
-    Console.WriteLine("Wrong input");
-}
-else
-{
-    Wages.compute(choice);
-}
+Wages.compute();
--- wages.cs
+++ wages.cs
@@ -8,54 +8,50 @@ namespace EmployeeWage
     internal class Wages
     {
         static int day_hr;
+        static int WaPhr = 20;
         public int rndm()               //for geting a random number
         {
             Random ran = new Random();
             return (ran.Next(0, 2));               //randomly checking present or absent
         }
-        public static int wage(int ch)                //for calculating the daily wage through switch case
+        public static int wage()                //for calculating the daily wage through switch case
         {
-            int WaPhr = 20;
+            int ch = new Random().Next(0, 3);
+
             day_hr = 0;
             switch (ch)
             {
                 case 1:
                     day_hr = 4;
+                    Console.WriteLine("Employee is working Part Time");
                     break;
                 case 2:
                     day_hr = 8;
+                    Console.WriteLine("Employee is working Full Time");
                     break;
                 default:
                     day_hr = 0;
+                    Console.WriteLine("Absent");
                     break;
             }
             return (WaPhr * day_hr);
         }
-        public static void compute(int ch)                //for calculating the monthly wage 
+        public static void compute()                //for calculating the monthly wage 
         {
             int days = 0;
-            int totHrs = 4;                     //B/c time limit will only come if person is full time , then at the day 12th total hrs =96 , one more day and hrs=104 therefore to satisfy the condition of 100 hrs
-            int ToMonWag = 0;                  //Monthly working days
-            int wa = Wages.wage(ch);                                 //called static method wage
+            int totHrs = 0;                     //total hrs present or absent
+            int ToMonWag = 0;                  //total Monthly working wage
             while (days < 20 && totHrs < 100)    //loop for checking no. of hrs<101 and total daysless than 21
             {
-                Wages random = new Wages();         //creating object for class wage for non-static functions
-                int r = random.rndm();            //Calling a non-static function
-                if (r == 1)
+                ToMonWag = ToMonWag + (Wages.wage());
+                days++;
+                totHrs = totHrs + day_hr;
+                Console.WriteLine("days=" + days + " total hrs= " + totHrs + " Total wage till now = " + ToMonWag);
+                if (totHrs > 100)                           //If day <20 &is a full time employee(8 hrs) & till now hrs =96 then after this total hrs=104
                 {
-                    Console.WriteLine("Employee is Present today ");
-                    ToMonWag = ToMonWag + wa;
-                    days++;                //when employee is present increment the day number
-                    totHrs = totHrs + day_hr;
-                }
-                else
-                {
-                    Console.WriteLine("Employee is Absent today ");
-                    days++;                //when employee is absent increment the day number and day_hr but wage not calculated
-                    totHrs = totHrs + day_hr;
+                    ToMonWag = ToMonWag - ((totHrs - 100) * WaPhr);
                 }
             }
-
             Console.WriteLine("Total wage of the employee for the month is = Rs." + ToMonWag);  //total monthly wage of the employee
         }
     }
