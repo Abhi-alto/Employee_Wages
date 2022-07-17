afcf64c44af99dee6d7096c22c0ce2aa9ca9ee90
unknown <abhisheksrivastava9719@gmail.com>
2022-07-17 11:39:51 +0530

    [Abhishek] Added part time employee wage function in the employee Wage Problem

--- wages.cs
+++ wages.cs
@@ -15,8 +15,8 @@ namespace EmployeeWage
         public static int wagee()
         {
             int WaPhr = 20;
-            int F_DaHr = 8;
-            return (WaPhr * F_DaHr);
+            int P_DaHr = 4;
+            return (WaPhr * P_DaHr);
 
         }
     }
