a2b6323a78565f12af97805711f0fbaa3124be9b
unknown <abhisheksrivastava9719@gmail.com>
2022-07-17 11:33:33 +0530

    [Abhishek] Added  attendence function in the employee Wage Problem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Welcome to the employee wage problem");
+﻿using EmployeeWage;
+Console.WriteLine("Welcome to Employee Wage Computation program");
+int r = Wages.Rndm();                                                  //Calling a static function
+if (r == 1)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
---
+++ wages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EmployeeWage
+{
+    internal class Wages
+    {
+        public static int Rndm()
+        {
+            Random ran = new Random();
+            return (ran.Next(0, 2));
+        }
+    }
+}
